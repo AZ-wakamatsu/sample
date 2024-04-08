a5b081aff1c68de12585bd20b3e95654f0abcda7
wakamatsu <hidemitsu.wakamatsu@alnetz.co.jp>
2024-04-08 17:23:23 +0900

    最初のコミット

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Article from './components/Article';
+
+function App() {
+  return (
+   <div>
+    <Article
+      title={'あああ'}
+      content={'いいい'}
+    />
+    <Article
+      title={'いいい'}
+      content={'ううう'}
+    />
+    <Article
+    title={'ううう'}
+    content={'えええ'}
+  />
+   </div>
+   
+  );
+}
+
+export default App;
